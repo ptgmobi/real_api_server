ef4c864ca5321327d7043392a2caba8da61f42b1
董家宏 <dongjiahong@hotmail.com>
2018-12-26 14:16:05 +0800

    modify conf

--- code_deploy_scripts/after_install.sh
+++ code_deploy_scripts/after_install.sh
@@ -15,38 +15,10 @@ if [ ! -d "/pdata1/log/offer" ]; then
     mkdir -p /pdata1/log/offer
 fi
 
-pushd /opt/offer_server
+pushd /opt/real_api_server
 
-echo "after install: sync: s3://${bucket}/offer-server/conf/"
-aws s3 sync s3://${bucket}/offer-server/conf/ conf/
-
-apple_ip_filter_dir="/pdata1/offer_server_data/apple_ip_filter"
-if [ ! -d "${apple_ip_filter_dir}" ]; then
-    mkdir -p ${apple_ip_filter_dir}
-fi
-
-apple_pmt_dir="/pdata1/offer_server_data/bundles"
-if [ ! -d "${apple_pmt_dir}" ]; then
-    mkdir -p ${apple_pmt_dir}
-fi
-
-echo "after install: sync: s3://${bucket}/apple_bloom_filter/data/"
-aws s3 sync s3://${bucket}/apple_bloom_filter/data/ ${apple_ip_filter_dir}
-
-bf_md5=$(md5sum "${apple_ip_filter_dir}/apple_bloom_filter" | cut -d ' ' -f1)
-if [ "$bf_md5" != "9fe1a45f115e5fb8d049257f494c2020" ]; then
-    echo "md5 un-match, apple_ip_bloom_filter changed"
-    exit 1
-fi
-
-replace_pkg_name_dir="/pdata1/offer_server_data/replace_pkg_name"
-if [ ! -d "${replace_pkg_name_dir}" ]; then
-    mkdir -p ${replace_pkg_name_dir}
-fi
-
-echo "after install: sync: s3://${bucket}/offer-server/replace_pkg_name/"
-aws s3 sync s3://${bucket}/offer-server/replace_pkg_name/ ${replace_pkg_name_dir}
-
+echo "after install: sync: s3://${bucket}/real-api-server/conf/"
+aws s3 sync s3://${bucket}/real-api-server/conf/ conf/
 
 echo "make deps"
 make deps > build.log 2>&1 || (cat build.log && exit 1)
